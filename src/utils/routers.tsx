2bdff344e54ab7e01305094834433e3801ba87fb
JinPark <jinchul112@gmail.com>
2023-04-23 14:17:11 +0200

    feat: base name

--- src/utils/routers.tsx
+++ src/utils/routers.tsx
@@ -1,25 +1,30 @@
-import { BrowserRouter, createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { OptionContextProvider } from '../contexts';
 import App from '../App';
 import SubApp from '../SubApp';
 
-export const routers = createBrowserRouter([
+export const routers = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: (
+        <OptionContextProvider>
+          <App />
+        </OptionContextProvider>
+      ),
+    },
+    {
+      path: '/sub',
+      element: (
+        <OptionContextProvider>
+          <SubApp />
+        </OptionContextProvider>
+      ),
+    },
+  ],
   {
-    path: '/',
-    element: (
-      <OptionContextProvider>
-        <App />
-      </OptionContextProvider>
-    ),
-  },
-  {
-    path: '/sub',
-    element: (
-      <OptionContextProvider>
-        <SubApp />
-      </OptionContextProvider>
-    ),
-  },
-]);
+    basename: '/',
+  }
+);
 
 export default null;
